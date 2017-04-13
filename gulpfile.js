92fed482653b29b70c9966c183d15b2764f78b1b
joshinrai <joshinrai@163.com>
2017-04-13 21:22:57 +0800

    修改gulp代码构建
    修改gulp代码构建

--- gulpfile.js
+++ gulpfile.js
@@ -192,7 +192,7 @@ gulp.task('dev', function(done) {
 gulp.task('connect', function() {
   connect.server({
     root: process.cwd(),
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     port: 3000,
     livereload: true
   });
